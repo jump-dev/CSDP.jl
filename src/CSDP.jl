cf7e065e42c778cc1c51826878f79cc30d97edba
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2016-04-01 15:22:04 +0200

    load binary dependencies

--- src/CSDP.jl
+++ src/CSDP.jl
@@ -1,5 +1,11 @@
 module CSDP
 
-# package code goes here
+# Try to load the binary dependency
+if isfile(joinpath(dirname(@__FILE__),"..","deps","deps.jl"))
+    include("../deps/deps.jl")
+else
+    error("CSDP not properly installed. Please run Pkg.build(\"CSDP\")")
+end
+
 
 end # module
