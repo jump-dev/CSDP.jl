ee7407954a0ee1f95fea61cbadd314ca0009aaa2
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2016-04-04 11:26:48 +0200

    use enums

--- test/jl_ref.jl
+++ test/jl_ref.jl
@@ -3,7 +3,11 @@ if !isdefined(:libn)
 end
 run(`gcc -fPIC -shared -o $libn -std=c99 ref.c`)
 
+const OP1 = UInt32(0)
+const OP2 = UInt32(1)
+
 type S
+    c::UInt32
     n::Cint
     e::Ptr{Cdouble}
 end
@@ -12,7 +16,7 @@ end
 vec = Cdouble[1.0, 2.0]
 
 println("&vec = $(pointer(vec))")
-s = S(length(vec), pointer(vec))
+s = S(OP1, length(vec), pointer(vec))
 
 println(ccall((:hello,libn), Cdouble, ()))
 
--- test/ref.c
+++ test/ref.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
+enum category {OP1, OP2};
 
 struct S {
+  enum category c;
   int n;
   double *e;
 };
@@ -14,7 +16,17 @@ double hello() {
 
 
 double sum(struct S s) {
-  printf("Hello\n");
+  printf("sum(struct S)\n");
+  switch (s.c) {
+  case OP1:
+    printf("OP1\n");
+    break;
+  case OP2:
+    printf("OP2\n");
+    break;
+  default:
+    printf("s.category = %d\n", (int) s.c);
+  }
   double r = 0;
   printf("sum(s): s.n = %d, s.e = %p\n", s.n, s.e);
   for (int i = 0; i < s.n; i++) {
