64b8bca6f467400dde1739af42eb9b3060d56a9a
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2016-04-04 18:00:41 +0200

    call dasum directly in Julia's Lapack

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,12 @@ using CSDP
 using Base.Test
 
 vec = Cdouble[1.0, 2.0, 0.0, -1.0]
+l = length(vec)
+inc = 1
+const llapack = Libdl.dlpath(LinAlg.LAPACK.liblapack)
+const dasum = WORD_SIZE == 64 ? :dasum_64_ : :dasum_
+n1 = ccall((dasum, llapack), Float64, (Ptr{Int}, Ptr{Cdouble}, Ptr{Int}), &l, vec, &inc)
+@assert abs(n1 - 4) < 1e-15 "n1 = $n1"
+
 n1 = ccall( (:norm1, CSDP.csdp), Float64, (Int, Ptr{Cdouble}), length(vec), vec)
-
-@test abs(n1 - 4) < 1e-15
-
+@assert abs(n1 - 4) < 1e-15 "n1 = $n1"
