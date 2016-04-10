dfb63f8cf1c9667599c65cc82f129dca907413fe
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2016-04-10 22:18:23 +0200

    change into directory

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,15 +7,21 @@ inc = 1
 const llapack = Libdl.dlpath(LinAlg.LAPACK.liblapack)
 const dasum = endswith(splitext(llapack)[1], "64_") ? :dasum_64_ : :dasum_
 try
-n1 = ccall((dasum, llapack), Float64, (Ptr{Int}, Ptr{Cdouble}, Ptr{Int}), &l, vec, &inc)
-@assert abs(n1 - 4) < 1e-15 "n1 = $n1"
-
-n1 = ccall( (:norm1, CSDP.csdp), Float64, (Cint, Ptr{Cdouble}), length(vec), vec)
-@assert abs(n1 - 4) < 1e-15 "n1 = $n1"
+    n1 = ccall((dasum, llapack), Float64, (Ptr{Int}, Ptr{Cdouble}, Ptr{Int}), &l, vec, &inc)
+    @assert abs(n1 - 4) < 1e-15 "n1 = $n1"
 catch
     println(dasum)
     println(llapack)
     rethrow()
 end
 
-include("../examples/example.jl")
+n1 = ccall( (:norm1, CSDP.csdp), Float64, (Cint, Ptr{Cdouble}), length(vec), vec)
+@assert abs(n1 - 4) < 1e-15 "n1 = $n1"
+
+cwd = pwd()
+try
+    cd("../examples/")
+    include("example.jl")
+finally
+    cd(cwd)
+end
