a3f116e838bfc0b3d182f895d6fa1e4ebfd42b9a
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2016-11-07 21:12:33 +0100

    wip

--- deps/src/lapack.h
+++ deps/src/lapack.h
@@ -1,10 +1,15 @@
 /* Copied from https://github.com/xianyi/OpenBLAS/blob/develop/common.h */
 
 #if defined(OS_WINDOWS) && defined(__64BIT__)
-typedef long long BLASLONG;
+  typedef long long BLASLONG;
 #else
 typedef long BLASLONG;
 #endif
 
-typedef BLASLONG integer;
+#if __WORDSIZE == 64
+  typedef int integer;
+#else
+  typedef BLASLONG integer;
+#endif
+
 typedef double doublereal;
