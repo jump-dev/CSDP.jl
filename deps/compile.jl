bfeea39cab812017479dfdaf345b45bbce1fe427
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2016-11-10 12:37:51 +0100

    doc find_obj

--- deps/compile.jl
+++ deps/compile.jl
@@ -1,24 +1,37 @@
 using Glob.glob
 
+const FORTRAN_FUNCTIONS =
+    [:dnrm2, :dasum, :ddot, :idamax, :dgemm, :dgemv, :dger,
+     :dtrsm, :dtrmv, :dpotrf, :dpotrs, :dpotri, :dtrtri]
+
+"""
+    find_obj([Makefile])
+
+Parse the original Csdp `Makefile` and return all the object names
+(without extension `.o`) for `libsdp`.
+Remark: You cannot use the Makefile directly as it produces only a
+static library.
+"""
 function find_obj(makefile_path=Makefile)
-    # patch: symlink debugging source
-    makefile = readstring(makefile_path)
+     makefile = readstring(makefile_path)
     m = match(r"libsdp\.a\:(.+)", makefile)
     m != nothing || error("Could not find `libsdp.a` target in '$makefile_path'")
     objs = matchall(r"\w+\.o", m.captures[1])
     objs = String[splitext(o)[1] for o in [objs; basename(patchf)]]
 end
 
-function patch_int()
+function patch_int(; verbose::Bool = false)
     let patchsrc = "$srcdir/$(basename(patchf))"
         isfile(patchsrc) || cp(patchf, patchsrc)
     end
     if JULIA_LAPACK
-        info("Patching INT --> integer")
+        info("Patching int --> integer")
         cfiles = [glob("*.c", srcdir); [joinpath(srcdir, "..", "include", "$d.h")
-                                        for d in ["declarations", "blockmat", "parameters"]]]
+                                        for d in ["declarations",
+                                                  "blockmat",
+                                                  "parameters"]]]
         for cfile in cfiles
-            # println(cfile)
+            if verbose; println(cfile); end
             content = readstring(cfile)
             for (re,subst) in
                 [(r"int ([^(]+);", s"integer \1;"),
@@ -27,8 +40,12 @@ function patch_int()
                  (r"integer mycompare", s"int mycompare"),
                  (r"\(int\)", s"(integer)"),
                  (r"%d", s"%ld"),
-                 (r"%2d", s"%2ld")]
+                 (r"%2d", s"%2ld"),
+                 (r" int ", s" integer ")]
                 content = replace(content, re, subst)
+            end
+            open(cfile, "w") do io
+                print(io, content)
             end
         end
     end
@@ -43,8 +60,7 @@ function compile_objs(JULIA_LAPACK=JULIA_LAPACK)
         info(libs)
         if endswith(LinAlg.LAPACK.liblapack, "64_")
             push!(cflags, "-march=x86-64", "-m64", "-Dinteger=long")
-            for f in [:dnrm2, :dasum, :ddot, :idamax, :dgemm, :dgemv, :dger,
-                      :dtrsm, :dtrmv, :dpotrf, :dpotrs, :dpotri, :dtrtri]
+            for f in FORTRAN_FUNCTIONS
                 let ext=string(BLAS.@blasfunc "")
                     push!(cflags, "-D$(f)_=$(f)_$ext")
                 end
@@ -54,11 +70,6 @@ function compile_objs(JULIA_LAPACK=JULIA_LAPACK)
     else
         libs = ["-l$l" for l in ["blas", "lapack"]]
         @static if is_windows() unshift!(libs, "-L$libdir") end
-        # libs = @static is_windows() ? ["-L$libdir", "-latlas"] : ["-l$l" for l in ["blas", "lapack"]]
-        # @static if is_windows()
-        #     unshift!(libs, "-march=x86-64")
-        #     push!(cflags, "-march=x86-64")
-        # end
     end
 
 
