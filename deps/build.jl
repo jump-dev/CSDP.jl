2ae15e155a10b007a346f87209e35decf318dc3b
Benoît Legat <benoit.legat@gmail.com>
2016-12-07 15:11:09 +0100

    Improvements suggested by @tkelman

--- deps/build.jl
+++ deps/build.jl
@@ -42,7 +42,7 @@ provides(BuildProcess,
 
 # Prebuilt DLLs for Windows
 provides(Binaries,
-   URI("https://github.com/EQt/winlapack/blob/master/win-csdp-$(Sys.WORD_SIZE).7z?raw=true"),
+   URI("https://github.com/EQt/winlapack/blob/49454aee32649dc52c5b64f408a17b5270bd30f4/win-csdp-$(Sys.WORD_SIZE).7z?raw=true"),
    [csdp, lapack, blas], unpacked_dir="usr", os = :Windows)
 
 @BinDeps.install Dict(:csdp => :csdp)
