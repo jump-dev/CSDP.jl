3ebf6dbab6c0b42e90c695a682b497cf020b975f
Benoît Legat <benoit.legat@gmail.com>
2017-07-23 13:18:52 +0200

    Add bug.jl

---
+++ test/bug.jl
@@ -0,0 +1,11 @@
+using CSDP
+using MathOptInterface
+const MOI = MathOptInterface
+using SemidefiniteOptInterface
+const SOI = SemidefiniteOptInterface
+m = CSDP.CSDPSolverInstance()
+SOI.initinstance!(m, [-2], 0)
+SOI.setobjectivecoefficient!(m, -1., 1, 1, 1)
+SOI.setobjectivecoefficient!(m, -1., 1, 2, 2)
+MOI.optimize!(m)
+@show m.status
