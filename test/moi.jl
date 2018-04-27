17a98cb86f1fb21d6b97e23ca119e58542b62aeb
Benoît Legat <benoit.legat@gmail.com>
2018-04-27 11:09:41 +0200

    Updates moi tests

--- test/moi.jl
+++ test/moi.jl
@@ -1,25 +1,16 @@
 using MathOptInterface
 const MOI = MathOptInterface
 const MOIT = MOI.Test
+const MOIB = MOI.Bridges
+
 const MOIU = MOI.Utilities
-
 MOIU.@model SDModelData () (EqualTo, GreaterThan, LessThan) (Zeros, Nonnegatives, Nonpositives, PositiveSemidefiniteConeTriangle) () (SingleVariable,) (ScalarAffineFunction,) (VectorOfVariables,) (VectorAffineFunction,)
-
-using MathOptInterfaceBridges
-const MOIB = MathOptInterfaceBridges
-
-MOIB.@bridge SplitInterval MOIB.SplitIntervalBridge () (Interval,) () () () (ScalarAffineFunction,) () ()
-MOIB.@bridge SOCtoPSDC MOIB.SOCtoPSDCBridge () () (SecondOrderCone,) () () () (VectorOfVariables,) (VectorAffineFunction,)
-MOIB.@bridge RSOCtoPSDC MOIB.RSOCtoPSDCBridge () () (RotatedSecondOrderCone,) () () () (VectorOfVariables,) (VectorAffineFunction,)
-MOIB.@bridge GeoMean MOIB.GeoMeanBridge () () (GeometricMeanCone,) () () () (VectorOfVariables,) (VectorAffineFunction,)
-MOIB.@bridge RootDet MOIB.RootDetBridge () () (RootDetConeTriangle,) () () () (VectorOfVariables,) (VectorAffineFunction,)
-
-const optimizer = CSDP.CSDPOptimizer(printlevel=0)
+const optimizer = MOIU.CachingOptimizer(SDModelData{Float64}(), CSDP.CSDPOptimizer(printlevel=0))
 const config = MOIT.TestConfig(atol=1e-4, rtol=1e-4)
 
 @testset "Linear tests" begin
-    MOIT.contlineartest(SplitInterval{Float64}(MOIU.CachingOptimizer(SDModelData{Float64}(), optimizer)), config)
+    MOIT.contlineartest(MOIB.SplitInterval{Float64}(optimizer), config)
 end
 @testset "Conic tests" begin
-    MOIT.contconictest(RootDet{Float64}(GeoMean{Float64}(RSOCtoPSDC{Float64}(SOCtoPSDC{Float64}(MOIU.CachingOptimizer(SDModelData{Float64}(), optimizer))))), config, ["psds", "rootdets", "logdet", "exp"])
+    MOIT.contconictest(MOIB.RootDet{Float64}(MOIB.GeoMean{Float64}(MOIB.RSOCtoPSD{Float64}(MOIB.SOCtoPSD{Float64}(optimizer)))), config, ["psds", "rootdets", "logdet", "exp"])
 end
