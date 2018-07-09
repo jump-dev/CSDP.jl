36260ad21cf7624bf8a55229a90d7a332d5754b3
Benoît Legat <benoit.legat@gmail.com>
2018-07-09 16:56:14 +0200

    Add unit tests

--- test/MOIWrapper.jl
+++ test/MOIWrapper.jl
@@ -18,9 +18,13 @@ MOIU.@model(SDModelData,
 const optimizer = MOIU.CachingOptimizer(SDModelData{Float64}(), CSDP.Optimizer(printlevel=0))
 const config = MOIT.TestConfig(atol=1e-4, rtol=1e-4)
 
-@testset "Linear tests" begin
+@testset "Unit" begin
+    #FIXME The solution of solve_blank_obj is arbitrary
+    MOIT.unittest(MOIB.SplitInterval{Float64}(optimizer), config, ["solve_blank_obj", "solve_qcp_edge_cases", "solve_qp_edge_cases"])
+end
+@testset "Continuous Linear" begin
     MOIT.contlineartest(MOIB.SplitInterval{Float64}(optimizer), config)
 end
-@testset "Conic tests" begin
+@testset "Continuous Conic" begin
     MOIT.contconictest(MOIB.RootDet{Float64}(MOIB.GeoMean{Float64}(MOIB.RSOCtoPSD{Float64}(MOIB.SOCtoPSD{Float64}(optimizer)))), config, ["psds", "rootdets", "logdet", "exp"])
 end
