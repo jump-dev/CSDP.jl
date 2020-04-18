3537a6202ed1a1aa621f3f2829a0849d1504a366
Benoît Legat <benoit.legat@gmail.com>
2020-04-18 20:12:19 +0200

    Use MySQL as example instead

--- deps/build.jl
+++ deps/build.jl
@@ -1,3 +1,9 @@
+# Inspired from https://github.com/JuliaDatabases/MySQL.jl/blob/36eaf2bfbbdd9a27c408d0b2a734fff0d81b63ad/deps/build.jl
+module Anon1 end
+module Anon2 end
+
+@static if VERSION < v"1.3.0"
+
 using BinaryProvider # requires BinaryProvider 0.3.0 or later
 
 # Parse some basic command-line arguments
@@ -8,20 +14,10 @@ products = [
     LibraryProduct(prefix, ["libcsdp"], :libcsdp)
 ]
 
-
-dependencies = [
-    "build_OpenBLAS32.v0.3.9.jl",
-    "build_CSDP.v6.2.0.jl"
-]
-
-for dependency in dependencies
-    # it's a bit faster to run the build in an anonymous module instead of
-    # starting a new julia process
-
-    # Build the dependencies
-    Mod = @eval module Anon end
-    Mod.include(dependency)
-end
+Anon1.include("build_OpenBLAS32.v0.3.9.jl")
+Anon2.include("build_CSDP.v6.2.0.jl")
 
 # Finally, write out a deps.jl file
 write_deps_file(joinpath(@__DIR__, "deps.jl"), products)
+
+end # VERSION
