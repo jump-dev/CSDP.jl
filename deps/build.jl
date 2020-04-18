c2161714f86326077fbb8bff21f9e80cf573e0c0
Benoît Legat <benoit.legat@gmail.com>
2020-04-18 18:30:31 +0200

    Updating build.jl similarly to FFMPEG and Cairo

--- deps/build.jl
+++ deps/build.jl
@@ -1,2 +1,27 @@
-include("build_OpenBLAS32.v0.3.9.jl")
-include("build_CSDP.v6.2.0.jl")
+using BinaryProvider # requires BinaryProvider 0.3.0 or later
+
+# Parse some basic command-line arguments
+const verbose = "--verbose" in ARGS
+const prefix = Prefix(get([a for a in ARGS if a != "--verbose"], 1, joinpath(@__DIR__, "usr")))
+
+products = [
+    LibraryProduct(prefix, ["libcsdp"], :libcsdp)
+]
+
+
+dependencies = [
+    "build_OpenBLAS32.v0.3.9.jl",
+    "build_CSDP.v6.2.0.jl"
+]
+
+for dependency in dependencies
+    # it's a bit faster to run the build in an anonymous module instead of
+    # starting a new julia process
+
+    # Build the dependencies
+    Mod = @eval module Anon end
+    Mod.include(dependency)
+end
+
+# Finally, write out a deps.jl file
+write_deps_file(joinpath(@__DIR__, "deps.jl"), products)
