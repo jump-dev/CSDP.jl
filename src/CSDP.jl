53b41fbdcb4601d75f07a4461b5f89a43367a0b7
Oscar Dowson <odow@users.noreply.github.com>
2022-02-28 10:10:12 +1300

    Prep for v1.0 release (#72)
    * Prep for v1.0 release
    
    * Update README
    
    * Move to MOI.Test
    
    * Report issue upstream
    
    * Exlude failing tests on GH Actions
    
    * Exclude failing tests on CI
    
    * Exclude more tests
    
    * Exclude failing test
    
    Co-authored-by: Benoît Legat <benoit.legat@gmail.com>

--- src/CSDP.jl
+++ src/CSDP.jl
@@ -3,14 +3,11 @@ module CSDP
 using LinearAlgebra # For Diagonal
 using SparseArrays # For SparseMatrixCSC
 
-if VERSION < v"1.3"
-    if isfile(joinpath(dirname(@__FILE__), "..", "deps", "deps.jl"))
-        include("../deps/deps.jl")
-    else
-        error("CSDP not properly installed. Please run Pkg.build(\"CSDP\")")
-    end
-else
-    import CSDP_jll: libcsdp
+import CSDP_jll
+
+function __init__()
+    global libcsdp = CSDP_jll.libcsdp
+    return
 end
 
 # This is the size of int used by the LAPACK library used by CSDP.
--- test/MOI_wrapper.jl
+++ test/MOI_wrapper.jl
@@ -1,90 +1,140 @@
+module TestCSDP
+
 using Test
-
 using MathOptInterface
-const MOI = MathOptInterface
-const MOIT = MOI.DeprecatedTest
-const MOIU = MOI.Utilities
-const MOIB = MOI.Bridges
-
 import CSDP
-const optimizer_constructor =
-    MOI.OptimizerWithAttributes(CSDP.Optimizer, MOI.Silent() => true)
-const optimizer = MOI.instantiate(optimizer_constructor)
 
-@testset "SolverName" begin
-    @test MOI.get(optimizer, MOI.SolverName()) == "CSDP"
+const MOI = MathOptInterface
+
+function runtests()
+    for name in names(@__MODULE__; all = true)
+        if startswith("$(name)", "test_")
+            @testset "$(name)" begin
+                getfield(@__MODULE__, name)()
+            end
+        end
+    end
+    return
 end
 
-const bridged =
-    MOI.instantiate(optimizer_constructor, with_bridge_type = Float64)
-const config = MOIT.Config(atol = 1e-4, rtol = 1e-4)
+function test_solver_name()
+    @test MOI.get(CSDP.Optimizer(), MOI.SolverName()) == "CSDP"
+end
 
-@testset "Options" begin
+function test_options()
     param = MOI.RawOptimizerAttribute("bad_option")
     err = MOI.UnsupportedAttribute(param)
     @test_throws err MOI.set(
-        optimizer,
+        CSDP.Optimizer(),
         MOI.RawOptimizerAttribute("bad_option"),
         0,
     )
 end
 
-@testset "Unit" begin
-    MOIT.unittest(
-        bridged,
-        config,
-        [
-            # `NUMERICAL_ERROR` on Mac: https://travis-ci.org/JuliaOpt/CSDP.jl/jobs/601302777#L217-L219
-            "solve_unbounded_model",
-            # `NumberOfThreads` not supported.
-            "number_threads",
-            # `TimeLimitSec` not supported.
-            "time_limit_sec",
-            # Quadratic functions are not supported
-            "solve_qcp_edge_cases",
-            "solve_qp_edge_cases",
-            # Integer and ZeroOne sets are not supported
-            "solve_integer_edge_cases",
-            "solve_objbound_edge_cases",
-            "solve_start_soc", # RSOCtoPSDBridge seems to be incorrect for dimension-2 RSOC cone.
-            "solve_zero_one_with_bounds_1",
-            "solve_zero_one_with_bounds_2",
-            "solve_zero_one_with_bounds_3",
+function test_runtests()
+    model = MOI.Utilities.CachingOptimizer(
+        MOI.Utilities.UniversalFallback(MOI.Utilities.Model{Float64}()),
+        MOI.instantiate(CSDP.Optimizer, with_bridge_type = Float64),
+    )
+    # `Variable.ZerosBridge` makes dual needed by some tests fail.
+    MOI.Bridges.remove_bridge(
+        model.optimizer,
+        MathOptInterface.Bridges.Variable.ZerosBridge{Float64},
+    )
+    MOI.set(model, MOI.Silent(), true)
+    MOI.Test.runtests(
+        model,
+        MOI.Test.Config(
+            rtol = 1e-3,
+            atol = 1e-3,
+            exclude = Any[
+                MOI.ConstraintBasisStatus,
+                MOI.VariableBasisStatus,
+                MOI.ObjectiveBound,
+                MOI.SolverVersion,
+            ],
+        ),
+        exclude = String[
+            # Empty constraint not supported
+            "test_linear_VectorAffineFunction_empty_row",
+            "test_conic_PositiveSemidefiniteConeTriangle",
+            # SLOW_PROGRESS
+            "test_quadratic_constraint_basic",
+            "test_quadratic_constraint_minimize",
+            # FIXME need to investigate
+            #  Expression: MOI.get(model, MOI.TerminationStatus()) == config.infeasible_status
+            #   Evaluated: MathOptInterface.OPTIMAL == MathOptInterface.INFEASIBLE
+            "test_conic_SecondOrderCone_INFEASIBLE",
+            # FIXME need to investigate
+            "test_objective_qp_ObjectiveFunction_edge_cases",
+            # See https://github.com/jump-dev/MathOptInterface.jl/issues/1761
+            "test_constraint_PrimalStart_DualStart_SecondOrderCone",
+            # FIXME investigate
+            # Internal library error: status=-1
+            "test_model_default_DualStatus",
+            "test_model_default_PrimalStatus",
+            # FIXME investigate
+            #  Expression: isapprox(MOI.get(model, MOI.ObjectiveValue()), T(2), config)
+            #   Evaluated: isapprox(6.0, 2.0, ...
+            "test_modification_delete_variables_in_a_batch",
+            # FIXME investigate
+            #   Expression: isapprox(MOI.get(model, MOI.ObjectiveValue()), objective_value, config)
+            #    Evaluated: isapprox(4.162643563176971e-9, 5.0
+            "test_objective_qp_ObjectiveFunction_zero_ofdiag",
+            # FIXME investigate
+            #  Expression: isapprox(MOI.get(model, MOI.ConstraintPrimal(), index), solution_value, config)
+            #   Evaluated: isapprox(9.973062820023415e-9, 1.0, ...
+            "test_variable_solve_with_lowerbound",
+            # TODO CSDP just returns an infinite ObjectiveValue
+            "test_unbounded_MIN_SENSE",
+            "test_unbounded_MIN_SENSE_offset",
+            "test_unbounded_MAX_SENSE",
+            "test_unbounded_MAX_SENSE_offset",
+            # TODO SDPT3 just returns an infinite DualObjectiveValue
+            "test_infeasible_MAX_SENSE",
+            "test_infeasible_MAX_SENSE_offset",
+            "test_infeasible_MIN_SENSE",
+            "test_infeasible_MIN_SENSE_offset",
+            "test_infeasible_affine_MAX_SENSE",
+            "test_infeasible_affine_MAX_SENSE_offset",
+            "test_infeasible_affine_MIN_SENSE",
+            "test_infeasible_affine_MIN_SENSE_offset",
+            # See https://github.com/jump-dev/MathOptInterface.jl/issues/1758
+            "test_model_copy_to_UnsupportedAttribute",
+            # FIXME The following are weird test failures that occur only on Github Actions for Mac OS but not Linux.
+            # It also seems to not be consistent between runs:
+            # Incorrect solution
+            "test_objective_ObjectiveFunction_VariableIndex",
+            "test_conic_SecondOrderCone_negative_post_bound",
+            "test_solve_result_index",
+            "test_modification_set_singlevariable_lessthan",
+            "test_conic_SecondOrderCone_nonnegative_initial_bound",
+            "test_objective_FEASIBILITY_SENSE_clears_objective",
+            # ALMOST_OPTIMAL
+            "test_conic_RotatedSecondOrderCone_VectorOfVariables",
+            # NUMERICAL_ERROR
+            "test_objective_ObjectiveFunction_blank",
+            "test_objective_ObjectiveFunction_duplicate_terms",
+            "test_modification_transform_singlevariable_lessthan",
+            "test_conic_SecondOrderCone_no_initial_bound",
+            "test_conic_SecondOrderCone_negative_initial_bound",
+            "test_solve_TerminationStatus_DUAL_INFEASIBLE",
+            "test_modification_set_singlevariable_lessthan:",
+            "test_modification_delete_variable_with_single_variable_obj",
+            #   Expression: MOI.get(model, MOI.TerminationStatus()) == MOI.DUAL_INFEASIBLE
+            #  Evaluated: MathOptInterface.INFEASIBLE == MathOptInterface.DUAL_INFEASIBLE
+            "test_conic_SecondOrderCone_negative_post_bound_2",
+            "test_conic_SecondOrderCone_negative_post_bound_3",
+            #  Expression: MOI.get(model, MOI.TerminationStatus()) == config.optimal_status
+            #   Evaluated: MathOptInterface.INFEASIBLE == MathOptInterface.OPTIMAL
+            "test_modification_const_scalar_objective",
+            "test_modification_coef_scalar_objective",
+            "test_modification_set_singlevariable_lessthan",
         ],
     )
+    return
 end
-@testset "Continuous Linear" begin
-    # See explanation in `MOI/test/Bridges/lazy_bridge_optimizer.jl`.
-    # This is to avoid `Variable.VectorizeBridge` which does not support
-    # `ConstraintSet` modification.
-    MOIB.remove_bridge(bridged, MOIB.Constraint.ScalarSlackBridge{Float64})
-    MOIT.contlineartest(bridged, config, [
-        # Finds `MOI.ALMOST_OPTIMAL` instead of `MOI.OPTIMAL`
-        "linear10b",
-        # Empty constraint
-        "linear15",
-    ])
-end
-@testset "Continuous Conic" begin
-    MOIT.contconictest(
-        bridged,
-        config,
-        [
-            # Finds `MOI.OPTIMAL` instead of `MOI.INFEASIBLE`.
-            "soc3",
-            # Empty constraint `c4`
-            "psdt2",
-            # See https://github.com/coin-or/Csdp/issues/11
-            "rotatedsoc1v",
-            # Missing bridges
-            "rootdets",
-            # Does not support power and exponential cone
-            "pow",
-            "dualpow",
-            "logdet",
-            "exp",
-            "dualexp",
-            "relentr",
-        ],
-    )
-end
+
+end  # module
+
+TestCSDP.runtests()
